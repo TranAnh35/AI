bf9a0f128dc643c3dbbe16beff4d80fd991dc3d9
TranAnh35 <tranleanh352004@gmail.com>
2024-10-30 13:36:03 +0700

    Update AI

--- Project/Chess/src/square.py
+++ Project/Chess/src/square.py
@@ -1,41 +1,47 @@
+from const import *
 
 class Square:
-    
-    ALPHACOLS = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
-    
+
+    ALPHACOLS = { 0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h' }
+
     def __init__(self, row, col, piece=None):
         self.row = row
         self.col = col
         self.piece = piece
         self.alphacol = self.ALPHACOLS[col]
-        
+
+    def __str__(self):
+        return '(' + str(self.row) + ', ' + str(self.col) + ')'
+
     def __eq__(self, other):
         return self.row == other.row and self.col == other.col
-    
+
+    # -------------
+    # OTHER METHODS
+    # -------------
+
     def has_piece(self):
         return self.piece != None
-    
-    def is_empty(self):
-        return not self.has_piece()
-    
+
     def has_team_piece(self, color):
-        return self.has_piece() and self.piece.color == color
+        return self.piece != None and self.piece.color == color
+
+    def has_rival_piece(self, color):
+        return self.piece != None and self.piece.color != color
     
-    def has_enemy_piece(self, color):
-        return self.has_piece() and self.piece.color != color
-    
-    def is_empty_or_enemy(self, color):
-        return self.is_empty() or self.has_enemy_piece(color)
-    
+    def isempty(self):
+        return self.piece == None
+
+    def isempty_or_rival(self, color):
+        return self.piece == None or self.piece.color != color
+
     @staticmethod
     def in_range(*args):
         for arg in args:
-            if arg < 0 or arg > 7:
-                return False
-        
+            if arg < 0 or arg > 7: return False
         return True
-
+    
     @staticmethod
     def get_alphacol(col):
-        ALPHACOLS = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
-        return ALPHACOLS[col]
+        alphacols = { 0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h' }
+        return alphacols[col]
