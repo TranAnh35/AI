a9f94f89b33b993593f21aa9775d0fa38d9bd9ed
TranAnh35 <tranleanh352004@gmail.com>
2024-11-01 17:29:31 +0700

    delete popup file

--- Project/TicTacToe/src/popup.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Popup:
-    def __init__(self, surface, winner=None, font_name="Comic Sans MS", font_size=36):
-        self.surface = surface
-        self.winner = winner
-        self.font = pygame.font.SysFont(font_name, font_size)
-        self.text = self.font.render(self.winner, True, (0, 0, 0))
-        self.text_rect = self.text.get_rect(center=(250, 250))
